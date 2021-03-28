91d6691b0f49426204840c99468acf11e6cbd89f
William Dumoulin <william@idt.be>
2021-03-28 17:45:21 +0200

    suppression py inutile

--- hello.py
+++
@@ -1,1 +0,0 @@
-print('Il like git')
--- model.py
+++ model.py
@@ -3,6 +3,9 @@ Dans ce script python on va fabriquer notre modèle.
 On utilise les librairies Tensorflow et Keras (comme en cours).
 Elles ont des RNN intégrées qui vont nous servir à analyser les vidéos.
 
+
+ça peut être bien de faire le modèle sur jupyter
+afin de pouvoir visualier chaque étape plus facilement
 '''
 
 import tensorflow as tf
@@ -20,6 +23,11 @@ Insertion des données
 #images.open
 #cv2.imread
 #Insertion des vidéos?
+#https://www.youtube.com/watch?v=fmga0i0MXuU&ab_channel=Weights%26Biases
+#à 3 minutes, il montre un générateur.
+#Le principe est d'amener les vidéos par paquet et non tout en un coup
+#Sinon ça prend trop de place dans la mémoire
+#Il combine des modèles aussi mais je n'ai pas vraiment suivi
 
 '''
 Traitement des données
