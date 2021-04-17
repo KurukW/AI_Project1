cc55ca88c3c09a5320a4a841ea182912212aaa3a
William Dumoulin <william@idt.be>
2021-04-17 12:32:01 +0200

    Live_predict Ok

--- live_predict.py
+++ live_predict.py
@@ -16,6 +16,8 @@ import numpy as np
 import time
 import threading
 from queue import Queue
+import sys #Afin d'arrêter le programme
+import pandas as pd
 
 
 #-------------------------------------------------------------------------------
@@ -48,16 +50,65 @@ def scale_by_pixels(img, x_min, x_max):
     return new_img
 
 
+
+#Thread
+def predict(model):
+    '''
+    Prédit un résultat de la queue et le remet dans la queue
+    '''
+    while True:
+        X = q_to_pred.get()
+        pred = model.predict(X)
+        q_pred.put(pred)
+
+def show_prediction():
+    '''
+    Affiche un résultat
+    '''
+    while True:
+        pred = q_pred.get()
+        print("Prediction : ",labels_n[pred.argmax()])
+        #print("somme de la prédiction",np.sum(pred)) #DEBUG
+        print("Nouvelle prediction:",pred) #DEBUG
+        #Si j'ai [nan,nan,...] c'est que le modele est cassé
+
+
+
+
 #-------------------------------------------------------------------------------
 '''
 Main
 
 '''
-fps = -1 #Changer le nombre d'FPS ne change pas le temps d'exécution donc
-        #mes opérations ne prennent pas de temps
-size = (80,60)
+
+#Choix du modele, attention que le modele doit exister
+fps = 8
+size = (40,30) #Sens inverse au nom du modèle
+nb_classes = 10
+epochs = 1
+batch_size = 20
+pack_size = 50
+learning_rate = 0.01
 
 
+#Import du modèle
+#Certains modèles sont cassés, il n'y a pas le .pb dedans alors il ne sait pas ouvrir
+
+
+
+param = f"_{fps}_{size[1]}_{size[0]}_{nb_classes}_{epochs}_{batch_size}_{pack_size}_{int(learning_rate*1000)}mili"
+type = 'model_LSTM'
+folder = 'Saved_model'
+full_path = folder + '\\' + type + param
+try:
+    model = keras.models.load_model(full_path)
+    #keras.models.load_model('Saved_model\\modele_stolen_compile')
+    print("modele importé avec succès")
+except:
+    print('''Erreur lors du chargement du modele,
+    vérifiez que les paramètres sont bons et que le modele existe''')
+    print("Je voulais importer le modele:",full_path)
+    sys.exit()
 
 
 cap = cv2.VideoCapture(0)
@@ -68,8 +119,15 @@ fps_actu = cap.get(cv2.CAP_PROP_FPS)
 if fps <= -1: fps = fps_actu #Je peux ne pas donner de fps et ça va prendre le nombre d'fps initial
 ecart_voulu = int(1000/fps)
 ecart_initial = int(1000/fps_actu)
+first_start = time.time()
 
 
+#Fabrication du dictionnaire
+labels_name = pd.read_csv("DATA\\labels_uses.csv")
+labels_n = {}
+for i,label in enumerate(labels_name.values):
+    labels_n[label[0]] = i
+    labels_n[i] = label[0]
 
 
 # Automatically grab width and height from video feed
@@ -86,6 +144,20 @@ imgs = []
 X = []
 count = 0
 to_mean = 0
+n_frames = int(fps*2.4)
+
+
+#queue
+q_to_pred = Queue() #X vers la prédiction
+q_pred = Queue() #pred vers l'affichage
+
+
+#Thread
+t_show = threading.Thread(target=show_prediction,daemon=True)
+t_show.start()
+t_pred = threading.Thread(target=predict,args=((model,)),daemon= True)
+t_pred.start()
+
 
 
 while True:
@@ -96,14 +168,16 @@ while True:
 
 
     #Récupère seulement certaines images
-    t_ms = cap.get(cv2.CAP_PROP_POS_MSEC)
+    #t_ms = cap.get(cv2.CAP_PROP_POS_MSEC)
+    now = time.time()
+    t_ms = (now-first_start)*1000
     modulo = t_ms % ecart_voulu
     if modulo < ecart_initial:
         #Isoler les images
         gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         prev_gray = cv2.cvtColor(prev,cv2.COLOR_BGR2GRAY)
         diff_gray = cv2.absdiff(gray,prev_gray)
-        #Preprocess avant Ajouter
+    #Preprocess avant d'ajouter
         #Réduire la taille
         resized = cv2.resize(diff_gray, dsize=size, interpolation=cv2.INTER_CUBIC)
         #Normaliser
@@ -111,15 +185,29 @@ while True:
         imgs.append(normalized)
 
 
-    if len(imgs) > 24:
+    cv2.imshow('frame',frame)
+
+    if len(imgs) > n_frames:
         #Récupère les x premières images et supprimes de la liste
-        X = imgs[:24]
-        del imgs[:24]
+        X = imgs[:n_frames]
+        del imgs[:n_frames]
+        #Traitement de X pour pouvoir prédire
+        X = np.array(X)
+        X = np.expand_dims(X, axis=len(X.shape)) #Ajoute un channel
+        X = np.expand_dims(X, axis = 0) #Ajoute une dimension qui signifie
+        #                                qu'on analyse des vidéos et non des images
+        #print("Taille de X qu'on va prédire",X.shape)
 
+        start_pred = time.time()
         #Predict X
+        q_to_pred.put(X)
+        #print(f"Il se passe {start_pred - end_pred} secondes entre deux predictions")
+
+        #pred = model.predict(X)
+        end_pred = time.time()
 
 
-    cv2.imshow('frame',diff_gray)
+
 
 
 
@@ -135,4 +223,6 @@ while True:
 # When everything done, release the capture and destroy the windows
 cap.release()
 cv2.destroyAllWindows()
+# t_show.join()
+# t_pred.join()
 #print(f"une exécution prend en moyenne  {to_mean/count} secondes")
